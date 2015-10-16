f98482540ac2b0f6887b9450ab82139423ccf800
Jaime Canillas <jaime.canillas@iesfernandoaguilar.es>
2015-10-16 18:51:15 +0200

    esto no funciona

--- mayor_cho.py
+++ mayor_cho.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
-mayor = 0
 num = int(raw_input('mete un numero positivo (si te aburres mete un negativo y terminas): '))
+mayor = num
 while num>=0:
 	if num > mayor:
 		mayor = num
 	num = int(raw_input('mete un numero positivo (si te aburres mete un negativo y terminas): '))
-print "El mayor del chorro números es " + str(mayor)
+if num<0: # la vaca que ríe
+	print "Nos hemos aburrido pronto"
+else:
+	print "El mayor del chorro números es " + str(mayor)
 		
 
 
